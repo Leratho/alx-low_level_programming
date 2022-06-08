7ddf8a5b4fc3a2292dd46e8b2a3c50e2d75dd99d
Leratho <Leratho.masha@gmail.com>
2022-06-08 13:46:36 -0700

    Fixing error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,9 @@
  *
  * Description: prints the alphabet 10 times
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
 */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int bet;
 
